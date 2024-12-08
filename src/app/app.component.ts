94e21d656c7c08c621b47abb5478a2dc19ae3e21
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2024-12-08 19:31:38 -0300

    Implementação da header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
